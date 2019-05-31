008565818f6cfea8bc01e9f09e5a6d7a0d59a09c
Nisha2018 <38083071+Nisha2018@users.noreply.github.com>
2019-05-31 20:28:20 +0530

    home section modified and code updated

--- portfolio.js
+++ portfolio.js
@@ -14,4 +14,11 @@ $(document).ready(function () {
     $(".close").hide();
   });
 
+  //Skills set Progress bar
+
+  $(".html").animate({ width: '95%' }, 2000).css("backgroundColor", "#4caf50");
+  $(".js").animate({ width: '70%' }, 2000).css("backgroundColor", "#2196f3");
+  $(".jquery").animate({ width: '75%' }, 2000).css("backgroundColor", "#808080");
+  $(".bootstrap").animate({ width: '75%' }, 2000).css("backgroundColor", "salmon");
+
 });
